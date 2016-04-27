59b00e07628aaead16a4ce3a004aad595d8931f6
miroop <mp@perennial.de>
2016-04-27 14:59:01 +0300

    change perspective manager to include folders in file list -- issue TS57

--- data/js/perspective.manager.js
+++ data/js/perspective.manager.js
@@ -216,47 +216,50 @@ define(function(require, exports, module) {
         path = dirList[i].path.replace(/(<([^>]+)>)/gi, ''); // sanitizing filepath
         title = TSCORE.TagUtils.extractTitle(filename);
 
-        if (dirList[i].isFile) {
-          ext = TSCORE.TagUtils.extractFileExtension(filename);
-          tags = TSCORE.TagUtils.extractTags(path);
+        //if (dirList[i].isFile) {
+        ext = TSCORE.TagUtils.extractFileExtension(filename);
+        tags = TSCORE.TagUtils.extractTags(path);
 
-          if (dirList[i].size) {
-            fileSize = dirList[i].size;
-          } else {
-            fileSize = "";
-          }
-
-          if (dirList[i].lmdt) {
-            fileLMDT = dirList[i].lmdt;
-          } else {
-            fileLMDT = '';
-          }
-
-          if (dirList[i].thumbPath) {
-            thumbPath = dirList[i].thumbPath;
-          } else {
-            thumbPath = '';
-          }
-
-          metaObj = {
-            thumbnailPath: thumbPath,
-            metaData: null
-          };
-
-          entry = {
-            "extension": ext,
-            "title": title,
-            "tags": tags,
-            "size": fileSize,
-            "lmdt": fileLMDT,
-            "path": path,
-            "name": filename,
-            "isDirectory": false,
-            "meta": metaObj
-          };
-          TSCORE.fileList.push(entry);
-          metaDataLoadingPromises.push(TSCORE.Meta.loadMetaFileJsonPromise(entry));
+        if (dirList[i].size) {
+          fileSize = dirList[i].size;
         } else {
+          fileSize = "";
+        }
+
+        if (dirList[i].lmdt) {
+          fileLMDT = dirList[i].lmdt;
+        } else {
+          fileLMDT = '';
+        }
+
+        if (dirList[i].thumbPath) {
+          thumbPath = dirList[i].thumbPath;
+        } else {
+          thumbPath = '';
+        }
+
+        metaObj = {
+          thumbnailPath: thumbPath,
+          metaData: null
+        };
+
+        entry = {
+          "extension": ext,
+          "title": title,
+          "tags": tags,
+          "size": fileSize,
+          "lmdt": fileLMDT,
+          "path": path,
+          "name": filename,
+          "isDirectory": !dirList[i].isFile,
+          "meta": metaObj
+        };
+        TSCORE.fileList.push(entry);
+        if (dirList[i].isFile) {
+          metaDataLoadingPromises.push(TSCORE.Meta.loadMetaFileJsonPromise(entry));
+        }
+        //} else {
+        if (!dirList[i].isFile) {  
           entry = {
             "path": path,
             "name": filename,
